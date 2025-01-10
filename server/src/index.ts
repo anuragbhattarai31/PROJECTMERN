2e9072e3f57699b24cabb00e1576c21dd01151ba
Anurag Bhattarai <anuragbhattarai31@gmail.com>
2025-01-09 22:47:08 -0600

    Some minute changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,19 +3,15 @@ import express, { Express } from "express";
 import mongoose from "mongoose";
 import financialRecordRouter from "./routes/financial-records";
 import cors from "cors";
-import dotenv from "dotenv";
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
-dotenv.config();
-
-
 app.use(express.json());
 app.use(cors());
 
-const mongoURI: string = "mongodb+srv://anuragbhattarai31:OWMwtTYUuJg3ohmx@financetracker.0acon.mongodb.net/";
-  
+const mongoURI: string =
+  "mongodb+srv://anuragbhattarai31:OWMwtTYUuJg3ohmx@financetracker.0acon.mongodb.net/";
 
 mongoose
   .connect(mongoURI)
